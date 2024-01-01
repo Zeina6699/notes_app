0bb9b4b48dac5a2da241dc33da46bf01fff5b1ca
Zeina6699 <zenahhamad25@gmail.com>
2024-01-01 05:16:03 -0500

    create Edit Note View

--- lib/view/widgets/customNoteItem.dart
+++ lib/view/widgets/customNoteItem.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notesapp/view/editNoteView.dart';
 import 'package:notesapp/view/widgets/showDeleteDialog.dart';
 
 class NoteItem extends StatelessWidget {
@@ -6,42 +7,51 @@ class NoteItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(left: 17, top: 17, bottom: 17),
-      decoration: BoxDecoration(
-          color: Colors.yellow, borderRadius: BorderRadius.circular(16)),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-              title: Text(
-                "Flutter Tips",
-                style: TextStyle(color: Colors.black, fontSize: 26),
-              ),
-              subtitle: Padding(
-                padding: const EdgeInsets.only(top: 16, bottom: 10),
-                child: Text("Build your career with zeina",
-                    style: TextStyle(
-                        color: Colors.black.withOpacity(.5), fontSize: 20)),
-              ),
-              trailing: IconButton(
-              icon: Icon( Icons.delete,color: Colors.black,size: 30,),
-                onPressed: () {
-                showDialog(
-                    context: context,
-                    builder: (context) {
-                      return showDeleteDialog();
-                    });
-              })),
-          Padding(
-            padding: const EdgeInsets.only(right: 20),
-            child: Text("June24 , 2001",
-                style: TextStyle(color: Colors.black.withOpacity(.4))),
-          )
-        ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (Context) {
+          return const EditNoteView();
+        }));
+      },
+      child: Container(
+        padding: EdgeInsets.only(left: 17, top: 17, bottom: 17),
+        decoration: BoxDecoration(
+            color: Colors.yellow, borderRadius: BorderRadius.circular(16)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+                title: Text(
+                  "Flutter Tips",
+                  style: TextStyle(color: Colors.black, fontSize: 26),
+                ),
+                subtitle: Padding(
+                  padding: const EdgeInsets.only(top: 16, bottom: 10),
+                  child: Text("Build your career with zeina",
+                      style: TextStyle(
+                          color: Colors.black.withOpacity(.5), fontSize: 20)),
+                ),
+                trailing: IconButton(
+                    icon: Icon(
+                      Icons.delete,
+                      color: Colors.black,
+                      size: 30,
+                    ),
+                    onPressed: () {
+                      showDialog(
+                          context: context,
+                          builder: (context) {
+                            return showDeleteDialog();
+                          });
+                    })),
+            Padding(
+              padding: const EdgeInsets.only(right: 20),
+              child: Text("June24 , 2001",
+                  style: TextStyle(color: Colors.black.withOpacity(.4))),
+            )
+          ],
+        ),
       ),
     );
   }
 }
-
-
